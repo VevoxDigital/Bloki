3b60dd56dc3d3d99b82bfd12876bd8f2c08985ce
Matthew Struble <matt@vevox.io>
2016-08-28 11:41:08 -0700

    Default yo app/test directory

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blokiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blokiApp
+ */
+angular.module('blokiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
