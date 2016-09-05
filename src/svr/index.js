665e4937361b53c02b8cf2e1e25018d5c4addf73
Matthew Struble <matt@vevox.io>
2016-09-05 15:28:41 -0700

    Temporary test daemon

--- src/svr/index.js
+++ src/svr/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs      = require('fs'),
+      path    = require('path'),
+      config  = require('nconf');
+
+const host = Object.keys(JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'cfg', 'daemons.json'))))[0];
+
+setTimeout(() => {
+  const socket = require('socket.io-client')('http://' + host + ':' + config.get('port') + '/daemon');
+
+  socket.on('connect', () => { console.log('conn'); });
+  socket.on('disconnect', () => { console.log('disconn'); });
+  socket.on('event', console.log);
+
+  global.TEST_SOCKET = socket;
+}, 1000);
